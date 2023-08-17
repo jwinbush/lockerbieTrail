12eba8453c7e96303f7c900765a9c610a306105a
Jay Winbush <jawonwinbush@gmail.com>
2023-08-17 18:27:33 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -444,7 +444,7 @@ function friend() {
   if (num > 50) {
     $(".ongoing-events").prepend("Riley respects the way " + wagon.characters[index].name + " rhymed \"swimmin' hole\" with \"brimmin' bowl.\" He fist-bumps you on your way into the building. <br><br><br>");
   } else {
-    $(".ongoing-events").prepend("Your rhyming skills were far too weak. Riley's nine little goblins (oh, did we not mention he has nine little goblins?) They tackle " + wagon.characters[index].name + " and carry them back to Fountain Square. <br><br><br>");
+    $(".ongoing-events").prepend("Your rhyming skills were far too weak. Riley's nine little goblins (oh, did we not mention he has nine little goblins?) tackle " + wagon.characters[index].name + " and carry them back to Fountain Square. <br><br><br>");
     wagon.characters[index].health = 0
     wagon.characters[index].status = "Unresponsive"
   }
